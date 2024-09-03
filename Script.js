8ec6172070240f273f6b99c0b852ee55b6198170
Raphael Rodrigues Marçal <raphael.rmarcal2002@gmail.com>
2024-09-03 15:23:03 -0300

    Update 1.1
    Indentação e formatação do código posteriormente manipulado.

--- Script.js
+++ Script.js
@@ -1,22 +1,20 @@
-   <script>
-        function insert(num) /*função da inserção dos números*/ {
-            var numero = document.getElementById('resultado').innerHTML;
-            document.getElementById('resultado').innerHTML = numero + num;
-        }
+function insert(num) /*função da inserção dos números*/ {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
 
-        function clean() /*função de limpar a caixa de resultado*/ {
-            document.getElementById('resultado').innerHTML = "";
-        }
+function clean() /*função de limpar a caixa de resultado*/ {
+    document.getElementById('resultado').innerHTML = "";
+}
 
-        function back() /*função de voltar*/ {
-            var resultado = document.getElementById('resultado').innerHTML;
-            document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
-        }
+function back() /*função de voltar*/ {
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
+}
 
-        function calcular() {
-            var resultado = document.getElementById('resultado').innerHTML;
-            if (resultado) {
-                document.getElementById('resultado').innerHTML = eval(resultado)
-            }
-        }
-    </script>
+function calcular() {
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
